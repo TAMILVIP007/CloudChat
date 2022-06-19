d6f0a45c4c065142583c6716b922dd16a7f328cb
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-06-19 14:26:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ load_dotenv()
 api_id = getenv('API_ID')
 api_hash = getenv('API_HASH')
 token = getenv('BOT_TOKEN')
-db = getenv('MONGO_URI')
+url = getenv('MONGO_URI')
 
 bot = TelegramClient("asst", api_id, api_hash).start(bot_token=token)
-client = MongoClient(db)["chat"]["info"]
-devs = list(getenv('DEVS'))
+cli = MongoClient(url, tls=True, tlsAllowInvalidCertificates=True)
+db = cli["cloud"]
+client = db.chats
+devs = 2107137268
 
 
 def get_chat(chat_id: int):
@@ -41,22 +43,22 @@ def get_link(chat_id: int):
     return get_chat(chat_id)["link"]
 
 @bot.on(events.NewMessage(pattern="/add", from_users=devs))
-async def add_chat(e):
+async def add_(e):
     try:
         chat_id = e.chat_id
         msg_id = e.id
-        link = (await client(functions.messages.ExportChatInviteRequest(chat_id))).link
         backup_chat_id = e.raw_text.split(" ")[1]
-        backup_link = (await client(functions.messages.ExportChatInviteRequest(int(backup_chat_id)))).link
         id = e.chat.title.split(" ")[-1]
+        link = (await bot(functions.messages.ExportChatInviteRequest(int(chat_id)))).link
+        backup_link = (await bot(functions.messages.ExportChatInviteRequest(int(backup_chat_id)))).link
         add_chat(chat_id, msg_id, link, backup_link, backup_chat_id, id)
         await e.reply("Added chat\nchat id : {}\nlink : {}\nbackup chat id : {}\nbackup link : {}\nid : {}".format(chat_id, link, backup_chat_id, backup_link, id))
-    except Exception as e:
-        await e.reply("Error : {}".format(e))
+    except Exception as a:
+        await e.reply("Error : {}".format(a))
         
 
 @bot.on(events.NewMessage(pattern="/remove", from_users=devs))
-async def remove_chat(e):
+async def rm_chat(e):
     try:
         chat_id = e.chat_id
         remove_chat(chat_id)
